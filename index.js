b0487ed40db7120e4df979a4abc362ea8923fc58
dmhlupin <45971646+dmhlupin@users.noreply.github.com>
2023-01-11 17:28:20 +0300

    HW2

--- index.js
+++ index.js
@@ -1,65 +1,108 @@
+import EventEmitter from "events";
 
-// Импортируем библиотеку colors
-import colors from 'colors';
 
-// Функция определения простых чисел из диапазона. На входе первое и последнее числа диапазона. На выходе массив значений.
-const simpleDig = (a, b) => {
-    const array = [];
-    const limit = Math.sqrt(b)
-    const result = [];
 
-    for (let i = a; i <= b; i++) {
-        let simple = true;
-        if(i == 0 || i == 1) {
-            simple = false;
-        }
-        for (let j = 2; (j <= i/2 && simple); j++) {
-            if (i % j == 0) {
-                simple = false;
-            }
-        }
-        if (simple) {
-            result.push(i);
-        }
-        
-    }
-    return result;
+const diffDateToObj = (futureDate) => {
+    const currDate = new Date();
+    let delta = (futureDate - currDate) / 1000;
+
+    // расчет количества дней и вычитание из разницы
+    const days = Math.floor(delta / 86400);
+    delta -= days * 86400;
+
+    // расчет количества часов и вычитание из разницы
+    const hours = Math.floor(delta / 3600) % 24;
+    delta -= hours * 3600;
+
+    // расчет количества минут и вычитание из разницы
+    const minutes = Math.floor(delta / 60) % 60;
+    delta -= minutes * 60;
+
+    // расчет количества оставшихся секунд
+    
+    const seconds = Math.floor(delta % 60);
+
+    return {days: days, hours: hours, minutes: minutes, seconds: seconds}
 }
 
-
-// Функция для организации вывода на экран
-const output = (a, b) => {
-    const message = '';
-    let outputArr = [];
-
-    if (Number.isInteger(a) && Number.isInteger(b)) {
-        if(a == b) {
-            console.log(colors.yellow("вы ввели одинаковые значения!"));
-        } else {
-            if(a > b){
-                outputArr = simpleDig(b,a);
-            } 
-            else outputArr = simpleDig(a,b);
-            if(!outputArr.length) {
-                console.log(colors.red("В диапазоне нет простых чисел!"))
-            } else {
-                for (let i = 0; i < outputArr.length; i = i+3) {
-                    console.log(colors.green(outputArr[i]));
-                    if(i+1 < outputArr.length) {
-                        console.log(colors.yellow(outputArr[i+1]));
-                    }
-                    if(i+2 < outputArr.length) {
-                        console.log(colors.red(outputArr[i+2]));
-                    }
-                }
-            }
-        }
-    }
-    else console.log( colors.red("Неправильный ввод данных!"));
+const checkDate = (diff) => {
+    if (diff.days > 0 && diff.hours > 0 && diff.minutes > 0 && diff.seconds > 0) {
+        return true
+    } else return false
 }
 
-// получение значений из терминала
+//`${days} дней - ${hours} часов - ${minutes} минут - ${seconds} секунд!`
+
+class Handler {
+    static timerRun(payload) {   
+        console.log(diffDateToObj(payload));
+        if(!checkDate(diffDateToObj(payload))) {
+            // генерируем событие Стоп
+            emitterObject.emit('timerStop', payload)
+        } else  
+        setInterval(() => console.log(diffDateToObj(payload)), 1000);
+    }
+    static timerStop(payload) {
+        console.log(`Time is up!`);
+    }
+}
+
+class myEmitter extends EventEmitter {};
+
+const emitterObject = new myEmitter();
+
+
+// создадим массив таймеров, пока пустой
+
+const timers = [];
+
+
+// создадим класс для таймера
+class Timer {
+    constructor(params) {
+        this.payload = params.payload;
+        this.condition = params.condition;
+    }
+}
+
+// генератор нового таймера
+
+const generateNewTimer = (data) => {
+    const futureDate = new Date(parseInt(data[3]),parseInt(data[2]) - 1, parseInt(data[1]), parseInt(data[0]), 0 , 0);
+    const params =  
+    {
+        payload: futureDate,
+        condition: 'timerRun'
+    }
+    
+    return new Timer(params);
+}
+
+// Подписываем обработчика на события
+
+emitterObject.on('timerRun', Handler.timerRun);
+emitterObject.on('timerStop', Handler.timerStop);
+
+
+
+// Получение значений из терминала
 const args = process.argv.slice(2);
 
-// вызов 
-output(parseInt(args[0]), parseInt(args[1]));
+// Преобразуем данные, полученные из терминала и создадим экземпляр таймера, который положим в массив таймеров
+
+args.forEach((item) => {
+    const data = item.split('-');
+    if(data.length != 4) {
+        console.log('Неверный формат!');
+    } else {
+        
+        const timer = generateNewTimer(data);
+        timers.push(timer);
+
+        // сгенерируем событие
+        emitterObject.emit('timerRun', timer.payload)
+    
+    }
+})
+
+
